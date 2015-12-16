6c2d836f4287df123a20060fa9810d0122b8b9a0
Jason Song <Jasonsong74@126.com>
2015-12-16 22:03:25 +0800

    Check Webhook

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WeiXin.Handler;
+namespace WeiXin
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.MessageHandlers.Add(new MessageHandler1());
+            config.MessageHandlers.Add(new MessageHandler2());
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "weixinapi",
+                defaults: new { id = RouteParameter.Optional }
+            );
+           
+            
+        }
+    }
+}
---
+++ Handler/MessageHandler1.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeiXin.Handler
+{
+    public class MessageHandler1 : DelegatingHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+
+            Trace.WriteLine("Process request");
+            // .WriteLine("Process request");
+            // Call the inner handler.
+            var response = await base.SendAsync(request, cancellationToken);
+            Debug.WriteLine("Process response");
+            return response;
+        }
+    }
+}
